281b7325bd144a41a925fe3a232e10b2fee3e42e
felix-huesken <felix.huesken@gmail.com>
2025-10-22 08:19:29 +0000

    Fixes

--- extension.js
+++ extension.js
@@ -51,6 +51,7 @@ export default class PastePresets extends Extension {
 		this._indicator = null;
 		this._settings = null;
 		this.keyboard = null;
+		this._pastingKeypressTimeout = null;
 	}
 
 	enable() {
@@ -58,20 +59,6 @@ export default class PastePresets extends Extension {
 		this._settings = this.getSettings();
 		this._settings.set_string("path", this.dir.get_path());
 		this._indicator = new PanelMenu.Button(0.0, "PanelButton", false);
-
-		let icon = new St.Icon({
-			icon_name: "insert-text-symbolic",
-			style_class: "system-status-icon",
-		});
-
-		this._indicator.add_child(icon);
-		Main.panel.addToStatusArea("Paste Presets", this._indicator);
-
-		// Reload the text entries from the text file and re-build the menu when it's opened
-		this._indicator.connect("button-press-event", (actor, event) => {
-			this._createMenu();
-		});
-
 		this._createMenu();
 
 		Main.wm.addKeybinding(
@@ -81,6 +68,14 @@ export default class PastePresets extends Extension {
 			Shell.ActionMode.ALL,
 			this._toggleMenu.bind(this)
 		);
+
+		let icon = new St.Icon({
+			icon_name: "insert-text-symbolic",
+			style_class: "system-status-icon",
+		});
+
+		this._indicator.add_child(icon);
+		Main.panel.addToStatusArea("Paste Presets", this._indicator);
 	}
 
 	_toggleMenu() {
@@ -95,12 +90,16 @@ export default class PastePresets extends Extension {
 			selectionCounter++;
 			if (selectionCounter >= presets.length) {
 				menu.close();
-				this._createMenu();
+				selectionCounter = 0;
 				menu.open();
 			}
 		} else {
-			this._createMenu();
+			selectionCounter = 0;
 			menu.open();
+			if (this._pastingKeypressTimeout) {
+				clearTimeout(this._pastingKeypressTimeout);
+				this._pastingKeypressTimeout = null;
+			}
 			this._pastingKeypressTimeout = setTimeout(() => {
 				this.keyboard.press(Clutter.KEY_Down);
 				this.keyboard.release(Clutter.KEY_Down);
@@ -108,60 +107,62 @@ export default class PastePresets extends Extension {
 		}
 	}
 
-	_createMenu() {
-		// Destroy and re-build the menu with new text entries
-		if (this._popUpMenu) {
-			this._popUpMenu.destroy();
-		}
-
+	async _createMenu() {
 		selectionCounter = 0;
 
 		this._popUpMenu = new PopupMenu.PopupMenuSection();
 		this._indicator.menu.addMenuItem(this._popUpMenu);
+		try {
+			presets = await this._readLinesFromFile();
 
-		presets = this._readLinesFromFile();
+			for (let i = 0; i < presets.length; i++) {
+				let itemLabel = presets[i];
+				if (presets[i].length > 50) {
+					itemLabel = presets[i].slice(0, 50) + "...";
+				}
+				let item = new PopupMenu.PopupMenuItem(itemLabel);
 
-		for (let i = 0; i < presets.length; i++) {
-			let itemLabel = presets[i];
-			if (presets[i].length > 50) {
-				itemLabel = presets[i].slice(0, 50) + "...";
-			}
-			let item = new PopupMenu.PopupMenuItem(itemLabel);
+				item.connect("activate", () => {
+					// Get the clipboard and insert the currently selected text entry into it.
+					const clipboard = St.Clipboard.get_default();
+					clipboard.set_text(St.ClipboardType.CLIPBOARD, presets[i]);
 
-			item.connect("activate", () => {
-				// Get the clipboard and insert the currently selected text entry into it.
-				const clipboard = St.Clipboard.get_default();
-				clipboard.set_text(St.ClipboardType.CLIPBOARD, presets[i]);
+					this._indicator.menu.close();
 
-				this._indicator.menu.close();
-
-				/* 
-				Paste the selected text entry from the clipboard.
-				
-				The following code snippet is taken from:
-				https://github.com/Tudmotu/gnome-shell-extension-clipboard-indicator
-				 */
-				this._pastingKeypressTimeout = setTimeout(() => {
-					if (
-						this.keyboard.purpose ===
-						Clutter.InputContentPurpose.TERMINAL
-					) {
-						this.keyboard.press(Clutter.KEY_Control_L);
-						this.keyboard.press(Clutter.KEY_Shift_L);
-						this.keyboard.press(Clutter.KEY_Insert);
-						this.keyboard.release(Clutter.KEY_Insert);
-						this.keyboard.release(Clutter.KEY_Shift_L);
-						this.keyboard.release(Clutter.KEY_Control_L);
-					} else {
-						this.keyboard.press(Clutter.KEY_Shift_L);
-						this.keyboard.press(Clutter.KEY_Insert);
-						this.keyboard.release(Clutter.KEY_Insert);
-						this.keyboard.release(Clutter.KEY_Shift_L);
+					/* 
+					Paste the selected text entry from the clipboard.
+					
+					The following code snippet is taken from:
+					https://github.com/Tudmotu/gnome-shell-extension-clipboard-indicator
+					*/
+					if (this._pastingKeypressTimeout) {
+						clearTimeout(this._pastingKeypressTimeout);
+						this._pastingKeypressTimeout = null;
 					}
-				}, 100);
-			});
+					this._pastingKeypressTimeout = setTimeout(() => {
+						if (
+							this.keyboard.purpose ===
+							Clutter.InputContentPurpose.TERMINAL
+						) {
+							this.keyboard.press(Clutter.KEY_Control_L);
+							this.keyboard.press(Clutter.KEY_Shift_L);
+							this.keyboard.press(Clutter.KEY_Insert);
+							this.keyboard.release(Clutter.KEY_Insert);
+							this.keyboard.release(Clutter.KEY_Shift_L);
+							this.keyboard.release(Clutter.KEY_Control_L);
+						} else {
+							this.keyboard.press(Clutter.KEY_Shift_L);
+							this.keyboard.press(Clutter.KEY_Insert);
+							this.keyboard.release(Clutter.KEY_Insert);
+							this.keyboard.release(Clutter.KEY_Shift_L);
+						}
+					}, 100);
+				});
 
-			this._popUpMenu.addMenuItem(item);
+				this._popUpMenu.addMenuItem(item);
+			}
+		} catch (e) {
+			log(e);
 		}
 	}
 
@@ -174,34 +175,43 @@ export default class PastePresets extends Extension {
 		Gio.AppInfo.launch_default_for_uri(file.get_uri(), null);
 	}
 
-	_readLinesFromFile() {
+	async _readLinesFromFile() {
 		let filePath = GLib.build_filenamev([
 			this.dir.get_path(),
 			"presets.txt",
 		]);
-		const file = Gio.file_new_for_path(filePath);
 
-		try {
-			let [ok, contents] = GLib.file_get_contents(filePath);
+		const file = Gio.File.new_for_path(filePath);
 
-			if (!ok) {
-				logError(new Error(`Failed to read ${filename}`));
-				return [];
-			}
-
-			let text = imports.byteArray.toString(contents);
-
+		const [ok, contents] = await new Promise((resolve, reject) => {
+			file.load_contents_async(null, (file, res) => {
+				try {
+					const [ok, contents] = file.load_contents_finish(res);
+					resolve([ok, contents]);
+				} catch (e) {
+					reject(e);
+				}
+			});
+		});
+		const text = new TextDecoder("utf-8").decode(contents);
+		if (ok) {
 			return text.split(/\r?\n/).filter((line) => line.trim().length > 0);
-		} catch (e) {
-			return [];
+		} else {
+			throw new Error("Failed to load file");
 		}
 	}
 
 	disable() {
+		this._settings = null;
+		this.keyboard = null;
+		if (this._pastingKeypressTimeout) {
+			clearTimeout(this._pastingKeypressTimeout);
+		}
+		this._pastingKeypressTimeout = null;
 		if (this._indicator) {
 			this._indicator.destroy();
 			this._indicator = null;
-			Main.wm.removeKeybinding("open");
 		}
+		Main.wm.removeKeybinding("open");
 	}
 }
